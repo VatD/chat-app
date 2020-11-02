e41b824b5a22ec19b3290ac1ee3beb1deec77f63
Vatsal <dhar.raja001@gmail.com>
2020-11-02 12:22:25 +0530

    added data persistance and improved user storage

--- server/utils/users.js
+++ server/utils/users.js
@@ -1,33 +1,35 @@
-const users = [];
+const users = new Map();
 
 const addUser = ({ id, username, room }) => {
-    if (!username || !room) return { error: "username and room required." };
+    if (!username || !room) return { error: "username and room required" };
 
     username = username.trim().toLowerCase();
     room = room.trim().toLowerCase();
 
-    const existingUser = users.find((user) => {
-        return user.room === room && user.username === username;
-    });
+    for (let val of users.values()) {
+        if (val.username === username && val.room === room)
+            return { error: "username in use" };
+    }
 
-    if (existingUser) return { error: "username in use" };
-
+    users.set(id, { username, room });
     const user = { id, username, room };
-    users.push(user);
     return user;
 };
 
 const removeUser = (id) => {
-    const index = users.findIndex((user) => user.id === id);
-    if (index !== -1) return users.splice(index, 1)[0];
+    const user = users.get(id);
+    users.delete(id);
+    return user;
 };
 
-const getUser = (id) => {
-    return users.find((user) => user.id === id);
-};
+const getUser = (id) => users.get(id);
 
 const getUsersInRoom = (room) => {
-    return users.filter((user) => user.room === room);
+    let roomUsers = [];
+    for (let [key, val] of users.entries()) {
+        if (val.room === room) roomUsers.push({ id: key, ...val });
+    }
+    return roomUsers;
 };
 
 module.exports = {
